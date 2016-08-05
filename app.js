3b5ab0330a62b34c9865433a5458f6f497856e56
MKFMIKU <mikumkf@outlook.com>
2016-08-05 22:17:01 +0800

    Add for Static middleware

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
 app.use('/api/account', account);
